80695c9d2ea3683ecf71eb0647358d1a69a7211f
sbb <mcdronoff@gmail.com>
2018-12-15 19:02:45 +0600

    Добавил стилей по минимуму, чтоб не совсем стрёмно было, и атрибут Required для ссылки, чтоб ещё на клиенте проверялась.

--- StaticResourceMonitor/Models/StaticResource/StaticResourceInfo.cs
+++ StaticResourceMonitor/Models/StaticResource/StaticResourceInfo.cs
@@ -1,11 +1,12 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace StaticResourceMonitor.Models.StaticResource
 {
     [StaticResourceInfoValidation(ErrorMessage = "Please enter a correct reference to a static resource.")]
     public class StaticResourceInfo
     {
-        [DisplayName("Reference to a static resource")]
+        [Required, DisplayName("Reference to a static resource")]
         public string Reference { get; set; }
     }    
 }
